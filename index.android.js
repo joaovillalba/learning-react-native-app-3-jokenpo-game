ac47c2018a3068d9c4c4f889ddd30b934a4b3fba
joaovillalba <joao.villalba@live.com>
2018-08-28 11:39:19 -0300

    Latest version.

--- index.android.js
+++ index.android.js
@@ -6,48 +6,35 @@
 
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
+  AppRegistry,  
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
-export default class app3 extends Component {
+class app3 extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      userChoose : '',
+      pcChoose : ''
+    }
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <View>
+        <Text>Escolha do Computador: {this.state.pcChoose}</Text>
+        <Text>Escolha do Usuário: {this.state.userChoose}</Text>
+        <Text>Resultado</Text>
+        <Button title='pedra' />        
+        <Button title='Papel' />        
+        <Button title='Tesoura' />        
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('app3', () => app3);
